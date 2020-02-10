c2c9eba741aac58ce95cdbb4c8960fe79ba4587f
Brian Gardner <bgardner@etsy.com>
2020-02-10 08:28:13 -0500

    Initial commit
    * Setup Android project with empty compose activity
    * Start creating the UserInfoRow composable function

---
+++ app/src/main/java/tech/briangardner/composeplayground/MainActivity.kt
@@ -0,0 +1,81 @@
+package tech.briangardner.composeplayground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.core.dp
+import androidx.ui.core.setContent
+import androidx.ui.core.sp
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Row
+import androidx.ui.layout.Spacing
+import androidx.ui.material.MaterialTheme
+import androidx.ui.text.TextStyle
+import androidx.ui.text.font.FontWeight
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                UserInfoRow("Brian Gardner", "@BrianGardnerDev", 1)
+            }
+        }
+    }
+}
+
+@Composable
+fun DisplayName(name: String) {
+    Text(
+        text=name,
+        modifier = Spacing(0.dp, 0.dp, 8.dp, 0.dp),
+        style = TextStyle(
+            color = Color.Black, // TODO update this with an appropriate theme color
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold
+        )
+    )
+}
+
+@Composable
+fun Handle(handle: String) {
+    Text(
+        text=handle,
+        modifier = Spacing(0.dp, 0.dp, 8.dp, 0.dp),
+        style = TextStyle(
+            color = Color.DarkGray, // TODO update this with an appropriate theme color
+            fontSize = 12.sp
+        )
+    )
+}
+
+@Composable
+fun PostTime(time: Long) {
+    // TODO: Add conversion for tweet time
+    Text(
+        text="7m",
+        style = TextStyle(
+            color = Color.DarkGray, // TODO update this with an appropriate theme color
+            fontSize = 12.sp
+        )
+    )
+}
+
+@Composable
+fun UserInfoRow(name: String, handle: String, time: Long) {
+    Row {
+        DisplayName(name)
+        Handle(handle)
+        PostTime(time)
+    }
+}
+
+@Preview
+@Composable
+fun TwitterPreview() {
+    MaterialTheme {
+        UserInfoRow("Brian Gardner", "@BrianGardnerDev", 1)
+    }
+}
