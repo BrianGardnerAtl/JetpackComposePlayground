deba6c117d5117629574bdf49794b54f3350d2ad
Brian Gardner <bgardner@etsy.com>
2020-02-12 10:08:53 -0500

    Make retweet and like buttons toggleable
    * Update compose version to dev04
    * Had to update the APIs to match the new naming
    * Also had to add a compose options block to set the kotlin compiler
    extension version so the resource loading would work

--- app/src/main/java/tech/briangardner/composeplayground/MainActivity.kt
+++ app/src/main/java/tech/briangardner/composeplayground/MainActivity.kt
@@ -5,10 +5,13 @@ import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.Model
 import androidx.compose.ambient
-import androidx.compose.unaryPlus
-import androidx.ui.core.*
+import androidx.ui.core.ContextAmbient
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
 import androidx.ui.foundation.Clickable
+import androidx.ui.foundation.selection.Toggleable
 import androidx.ui.graphics.Color
 import androidx.ui.graphics.vector.DrawVector
 import androidx.ui.layout.*
@@ -17,6 +20,8 @@ import androidx.ui.res.vectorResource
 import androidx.ui.text.TextStyle
 import androidx.ui.text.font.FontWeight
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import androidx.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,30 +32,60 @@ class MainActivity : AppCompatActivity() {
             time = 1,
             content = "This is a test tweet to see how things get rendered in the preview",
             commentCount = 100,
+            retweeted = false,
             retweetCount = 10,
+            liked = false,
             likeCount = 1000
         )
+        // This works but how can I make the Tweet class immutable?
+        val retweetToggle: ((Boolean) -> Unit) = { state ->
+            tweet.retweeted = state
+            tweet.retweetCount = if (state) {
+                tweet.retweetCount + 1
+            } else {
+                tweet.retweetCount - 1
+            }
+        }
+        val likedToggle: ((Boolean) -> Unit) = { state ->
+            tweet.liked = state
+            tweet.likeCount = if (state) {
+                tweet.likeCount + 1
+            } else {
+                tweet.likeCount - 1
+            }
+        }
         setContent {
             MaterialTheme {
-                TweetView(tweet)
+                TweetView(
+                    tweet,
+                    retweetToggle,
+                    likedToggle
+                )
             }
         }
     }
 }
 
 // State class for the Tweet data
+@Model
 data class Tweet(
     val displayName: String,
     val handle: String,
     val time: Long,
     val content: String,
     val commentCount: Int,
-    val retweetCount: Int,
-    val likeCount: Int
+    var retweeted: Boolean,
+    var retweetCount: Int,
+    var liked: Boolean,
+    var likeCount: Int
 )
 
 @Composable
-fun TweetView(tweet: Tweet) {
+fun TweetView(
+    tweet: Tweet,
+    retweetToggle: ((Boolean) -> Unit),
+    likeToggle: ((Boolean) -> Unit)
+) {
     Column {
         UserInfoRow(
             name = tweet.displayName,
@@ -60,8 +95,12 @@ fun TweetView(tweet: Tweet) {
         TweetContent(content = tweet.content)
         ActionRow(
             commentCount = tweet.commentCount,
+            retweeted = tweet.retweeted,
             retweetCount = tweet.retweetCount,
-            likeCount = tweet.likeCount
+            retweetToggle = retweetToggle,
+            liked = tweet.liked,
+            likeCount = tweet.likeCount,
+            likeToggle = likeToggle
         )
     }
 }
@@ -70,7 +109,7 @@ fun TweetView(tweet: Tweet) {
 @Composable
 fun UserInfoRow(name: String, handle: String, time: Long) {
     Row(
-        modifier = Spacing(8.dp)
+        modifier = LayoutPadding(8.dp)
     ) {
         DisplayName(name)
         Handle(handle)
@@ -82,7 +121,7 @@ fun UserInfoRow(name: String, handle: String, time: Long) {
 fun DisplayName(name: String) {
     Text(
         text=name,
-        modifier = Spacing(0.dp, 0.dp, 8.dp, 0.dp),
+        modifier = LayoutPadding(0.dp, 0.dp, 8.dp, 0.dp),
         style = TextStyle(
             color = Color.Black, // TODO update this with an appropriate theme color
             fontSize = 12.sp,
@@ -95,7 +134,7 @@ fun DisplayName(name: String) {
 fun Handle(handle: String) {
     Text(
         text=handle,
-        modifier = Spacing(0.dp, 0.dp, 8.dp, 0.dp),
+        modifier = LayoutPadding(0.dp, 0.dp, 8.dp, 0.dp),
         style = TextStyle(
             color = Color.DarkGray, // TODO update this with an appropriate theme color
             fontSize = 12.sp
@@ -125,7 +164,7 @@ fun TweetContent(content: String) {
             color = Color.Black, // TODO update this with an appropriate theme color
             fontSize = 12.sp
         ),
-        modifier = Spacing(8.dp)
+        modifier = LayoutPadding(8.dp)
     )
 }
 // endregion
@@ -134,45 +173,75 @@ fun TweetContent(content: String) {
 @Composable
 fun ActionRow(
     commentCount: Int,
+    retweeted: Boolean,
     retweetCount: Int,
-    likeCount: Int
+    retweetToggle: ((Boolean) -> Unit),
+    liked: Boolean,
+    likeCount: Int,
+    likeToggle: ((Boolean) -> Unit)
 ) {
-    val context = +ambient(ContextAmbient)
+    val context = ambient(ContextAmbient)
     Row(
-        modifier = Spacing(8.dp),
-        mainAxisSize = LayoutSize.Expand,
-        mainAxisAlignment = MainAxisAlignment.SpaceAround
+        modifier = LayoutWidth.Fill + LayoutPadding(8.dp),
+        arrangement = Arrangement.SpaceAround
     ) {
-        Comment(commentCount) { Toast.makeText(context, "Clicked on comment", Toast.LENGTH_SHORT).show() }
-        Retweet(retweetCount){  Toast.makeText(context, "Clicked on retweet", Toast.LENGTH_SHORT).show() }
-        Like(likeCount) { Toast.makeText(context, "Clicked on like", Toast.LENGTH_SHORT).show() }
+        // TODO update this to remove
+        Comment(commentCount, false) {
+            Toast.makeText(context, "Clicked on comment", Toast.LENGTH_SHORT).show()
+        }
+        Retweet(retweetCount, retweeted, retweetToggle)
+        Like(likeCount, liked, likeToggle)
         Share { Toast.makeText(context, "Clicked on share", Toast.LENGTH_SHORT).show() }
     }
 }
 
 @Composable
-fun Comment(count: Int, onClick : (() -> Unit)) {
-    InteractionImage(iconId = R.drawable.ic_comment, count = count, onClick = onClick)
+fun Comment(count: Int, commented: Boolean, onValueChange : ((Boolean) -> Unit)) {
+    ToggleImage(
+        iconId = R.drawable.ic_retweet,
+        count = count,
+        checked = commented,
+        onValueChange = onValueChange,
+        selectedColor = Color.LightGray
+    )}
+
+@Composable
+fun Retweet(count: Int, retweeted: Boolean, onValueChange : ((Boolean) -> Unit)) {
+    ToggleImage(
+        iconId = R.drawable.ic_retweet,
+        count = count,
+        checked = retweeted,
+        onValueChange = onValueChange,
+        selectedColor = Color.Green
+    )
 }
 
 @Composable
-fun Retweet(count: Int, onClick : (() -> Unit)) {
-    InteractionImage(iconId = R.drawable.ic_retweet, count = count, onClick = onClick)
+fun Like(count: Int, liked: Boolean, onValueChange : ((Boolean) -> Unit)) {
+    ToggleImage(
+        iconId = R.drawable.ic_like,
+        count = count,
+        checked = liked,
+        onValueChange = onValueChange,
+        selectedColor = Color.Red
+    )
 }
 
 @Composable
-fun Like(count: Int, onClick : (() -> Unit)) {
-    InteractionImage(iconId = R.drawable.ic_like, count = count, onClick = onClick)
-}
-
-@Composable
-fun InteractionImage(
+fun ToggleImage(
     @DrawableRes iconId: Int,
     count: Int,
-    onClick : (() -> Unit)
-    ) {
-    val icon = +vectorResource(iconId)
-    Clickable(onClick = onClick) {
+    checked: Boolean,
+    onValueChange: ((Boolean) -> Unit),
+    selectedColor: Color
+) {
+    val icon = vectorResource(iconId)
+    val color = if (checked) {
+        selectedColor
+    } else {
+        Color.LightGray
+    }
+    Toggleable(value = checked, onValueChange = onValueChange) {
         Row {
             Container(
                 expanded = true,
@@ -181,15 +250,15 @@ fun InteractionImage(
             ) {
                 DrawVector(
                     vectorImage = icon,
-                    tintColor = Color.LightGray
+                    tintColor = color
                 )
             }
             if (count > 0) {
                 Text(
-                    "$count",
-                    modifier = Spacing(8.dp, 0.dp, 0.dp, 0.dp),
+                    text = "$count",
+                    modifier = LayoutPadding(8.dp, 0.dp, 0.dp, 0.dp),
                     style = TextStyle(
-                        color = Color.LightGray,
+                        color = color,
                         fontSize = 18.sp
                     )
                 )
@@ -200,7 +269,7 @@ fun InteractionImage(
 
 @Composable
 fun Share(onClick : (() -> Unit)) {
-    val icon = +vectorResource(R.drawable.ic_share)
+    val icon = vectorResource(R.drawable.ic_share)
     Clickable(onClick = onClick) {
         Container(
             expanded = true,
@@ -225,10 +294,22 @@ fun TwitterPreview() {
         time = 1,
         content = "This is a test tweet to see how things get rendered in the preview",
         commentCount = 100,
+        retweeted = false,
         retweetCount = 10,
+        liked = false,
         likeCount = 1000
     )
+    val retweetToggle: ((Boolean) -> Unit) = { state ->
+        tweet.retweeted = state
+    }
+    val likedToggle: ((Boolean) -> Unit) = { state ->
+        tweet.liked = state
+    }
     MaterialTheme {
-        TweetView(tweet)
+        TweetView(
+            tweet,
+            retweetToggle,
+            likedToggle
+        )
     }
 }
